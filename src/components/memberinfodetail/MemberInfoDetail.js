65065251cfd19a5674d551a3233a15d5081f9171
DEM18 <morenodanielaeliana@gmail.com>
2020-08-10 16:36:29 -0300

    create member info details view

--- src/components/memberinfodetail/MemberInfoDetail.js
+++ src/components/memberinfodetail/MemberInfoDetail.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { getMemberById } from 'actions/index';
 
+import facebookIcon from 'assets/icons/facebook.png';
+import twitterIcon from 'assets/icons/twitter.png';
+
+import './MemberInfoDetail.scss';
+
 const mapStateToProps = ( state ) => {
     return {
-        memberById: state.charactersReducer.memberById
+        memberById: state.membersReducer.memberById
     }
 };
 
@@ -28,22 +33,36 @@ class MemberInfoDetail extends React.Component {
 
     render() {
         return(
-            <div>
-                <button onClick={this.onBackBtnClick}>
-                    <i class="fas fa-arrow-left"></i>
-                </button>
-                <ul>
-                {this.props.memberById.map( member => {
-                    return(
-                        <li key={member.id}>
-                            <label>{member.first_name}</label>
-                            <label>{member.last_name}</label>
-                            <label>{member.title}</label>
-                            <label>{member.date_of_birth}</label>
-                        </li>
-                    )
-                })}
-                </ul>
+            <div className="member-details">
+                <div className="member-details__item">
+                    <ul className="member-details__description">
+                    {this.props.memberById.map( member => {
+                        return(
+                            <li key={member.id}>
+                                <div className="member-details__header">
+                                    <button className="member-details__header--btn" onClick={this.onBackBtnClick}>
+                                        <i className="fas fa-arrow-left"></i>
+                                    </button>
+                                    <label className="member-details__description--item">{member.first_name} {member.last_name}</label>
+                                </div>
+                                <label className="member-details__description--item">{member.title}</label>
+                                <label className="member-details__description--item-date-of-birth">Date of birth: {member.date_of_birth}</label>
+                                <div className="member-details__description--item-social-network">
+                                    <img className="member-details__description--item-twitter" src={twitterIcon} alt="twitter"/>
+                                    <span>{member.twitter_account}</span>
+                                </div>
+                                <div className="member-details__description--item-social-network">
+                                    <img className="member-details__description--item-facebook" src={facebookIcon} alt="facebook"/>
+                                    <span>{member.facebook_account}</span>
+                                </div>
+                                <div>
+                                <a href={`${member.url}`}>Personal site</a>
+                                </div>
+                            </li>
+                        )
+                    })}
+                    </ul>
+                </div>
             </div>
         )
     }
